349ddb2af541a55f86f6986fccef36f352e38657
AJH <wangkenan@hongyi-info.com>
2020-01-15 17:37:27 +0800

    登录api

---
+++ service_login_api/src/main/java/com/fh/ServiceLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLoginApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLoginApiApplication.class, args);
+    }
+
+}
